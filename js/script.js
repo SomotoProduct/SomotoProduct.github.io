e70fd516ac1b95cf50ed9b1f03db21236c4283d4
eran <eran.darki@somoto.net>
2019-07-10 14:37:02 +0300

    Feed Layout Changes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 
   let totalRows = 4;
   let itemColumn = 0;
+  let numberOfItems = document.getElementsByClassName('wrapper')[0].getElementsByClassName('item').length;
+  let item = document.getElementsByClassName('wrapper')[0].getElementsByClassName('item');
 
   for (let rowCount = 0; rowCount < totalRows; rowCount++) {
     newColumn = document.createElement('div');
@@ -9,8 +11,8 @@
     document.getElementsByClassName('wrapper')[0].appendChild(newColumn);
   }
 
-  for (let itemCount = 0; itemCount < document.getElementsByClassName('item').length; itemCount++) {
-    document.getElementsByClassName('col')[itemColumn].appendChild(document.getElementsByClassName('item')[0]);
+  for (let itemCount = 0; itemCount < numberOfItems; itemCount++) {
+    document.getElementsByClassName('col')[itemColumn].appendChild(item[0]);
     if (itemColumn < totalRows - 1) {
       itemColumn++;
     } else {
